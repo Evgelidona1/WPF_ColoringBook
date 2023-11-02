c56060983628a2b57d8467eb4bad64a5561d0ed7
Evgelidona1 <evgelidona@yandex.ru>
2023-11-02 13:53:19 +0300

    kursach

---
+++ PAINTER/Picture1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PAINTER
+{
+    /// <summary>
+    /// Interaction logic for Picture1.xaml
+    /// </summary>
+    public partial class Picture1 : Page
+    {
+        public Picture1()
+        {
+            InitializeComponent();
+        }
+        private void ReturnButtonClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MyGame());
+            //Return.Content=new MainWindow();
+        }
+    }
+}
---
+++ PAINTER/Picture2.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PAINTER
+{
+    /// <summary>
+    /// Interaction logic for Picture2.xaml
+    /// </summary>
+    public partial class Picture2 : Page
+    {
+        public Picture2()
+        {
+            InitializeComponent();
+        }
+        private void ReturnButtonClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MyGame());
+            //Return.Content=new MainWindow();
+        }
+    }
+}
